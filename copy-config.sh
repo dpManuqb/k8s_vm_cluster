526096848e70fe8fb98ce7e9e10977d01d756c11
dpManuqb <manu_dp_3@hotmail.com>
2021-10-16 11:15:27 +0200

    Merged with master

--- copy-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-CLUSTER_IP=$NODE_NETWORK_BASE$NODE_IP_START
-
-#ssh -oStrictHostKeyChecking=no vagrant@$CLUSTER_IP "sudo cat /etc/kubernetes/admin.conf" > kubeconfig
-ssh -oStrictHostKeyChecking=no vagrant@$CLUSTER_IP "sudo cat /etc/kubernetes/admin.conf" > ~/.kube/config
