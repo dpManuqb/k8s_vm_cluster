1a5b4da67cb935e02335635ced7343f7f4e270d3
dpManuqb <manu_dp_3@hotmail.com>
2021-10-17 14:10:23 +0200

    Minor fixes!

--- scripts/wait-install.sh
+++ scripts/wait-install.sh
@@ -3,7 +3,7 @@
 echo "n" | ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -N "\n"
 cat $HOME/.ssh/id_rsa.pub | ssh -oStrictHostKeyChecking=no vagrant@$NODE_NETWORK_BASE$(($MASTER_IP)) "cat >> /home/vagrant/.ssh/authorized_keys"
 
-echo "\nWaiting Cluster to be ready..."
+echo "Waiting Cluster to be ready..."
 READY=$(ssh vagrant@$NODE_NETWORK_BASE$(($MASTER_IP)) 'grep ClusterReady /home/vagrant/provision.log | head -1')
 while [ "$READY" != "ClusterReady" ]
 do
