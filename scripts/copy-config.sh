0e83eb105e33cf6179f9d87496a32a0ea8d031bd
dpManuqb <manu_dp_3@hotmail.com>
2021-10-25 19:59:24 +0200

    Added almost all documentation first version.

--- scripts/copy-config.sh
+++ scripts/copy-config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Retrieving cluster config from master"
-#ssh -oStrictHostKeyChecking=no vagrant@$NODE_NETWORK_BASE$(($MASTER_IP)) "sudo cat /etc/kubernetes/admin.conf" > kubeconfig
-ssh -oStrictHostKeyChecking=no vagrant@$NODE_NETWORK_BASE$(($MASTER_IP)) "sudo cat /etc/kubernetes/admin.conf" > ~/.kube/config
+ssh -oStrictHostKeyChecking=no vagrant@$NODE_NETWORK_BASE$(($MASTER_IP)) "sudo cat /etc/kubernetes/admin.conf" > kubeconfig
+#ssh -oStrictHostKeyChecking=no vagrant@$NODE_NETWORK_BASE$(($MASTER_IP)) "sudo cat /etc/kubernetes/admin.conf" > ~/.kube/config
 echo "Done!"
