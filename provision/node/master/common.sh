68bbf29a62647f4229e0a2db73e85f87169ced9a
dpManuqb <manu_dp_3@hotmail.com>
2021-11-12 18:49:23 +0100

    Updated docker

--- provision/node/master/common.sh
+++ provision/node/master/common.sh
@@ -22,17 +22,30 @@ cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
-net.ipv6.ip_forward
+net.ipv6.ip_forward = 1
 EOF
 sudo sysctl --system
 
-sudo apt-get install -y docker.io
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+#sudo apt-get install -y docker.io
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
-  "exec-opts": ["native.cgroupdriver=systemd"]
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
 }
 EOF
 sudo systemctl restart docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 
 sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
